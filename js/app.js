63119b862efe788959a3a120dbfe713947bca3f7
dakyros <dakyros@users.noreply.github.com>
2026-09-04 07:29:58 -0600

    fix: setText uses #id selector so querySelector finds the element; remove debug alert

--- js/app.js
+++ js/app.js
@@ -911,7 +911,6 @@
     Promise.all([Db.fetchProductos(), Db.fetchCategorias(), Db.fetchUsuarios(), (Db.fetchPedidosAsAdmin || Db.fetchPedidos)()])
       .then(function (res) {
         var prod = res[0], cat = res[1], usr = res[2], ped = res[3];
-        alert('Dashboard debug: prod=' + prod.length + ' cat=' + cat.length + ' usr=' + usr.length + ' ped=' + ped.length);
         setText('cp-stat-productos', prod.length);
         setText('cp-stat-categorias', cat.length);
         setText('cp-stat-usuarios', usr.length);
@@ -933,7 +932,7 @@
   }
 
   function setText(id, val) {
-    var el = $(id);
+    var el = $('#' + id);
     if (el) el.textContent = val;
   }
 
